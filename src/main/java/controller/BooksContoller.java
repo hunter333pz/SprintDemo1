dabab8e581343dfce40509e0745da9e36a84d33f
Hunter333 <hunter333@abv.bg>
2018-08-25 23:10:24 +0300

    add books view

---
+++ src/main/java/controller/BooksContoller.java
@@ -0,0 +1,28 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.hunter333.webApp1.repository.BookRepository;
+
+/**
+ * 
+ * @author Hunter333
+ *
+ */
+@Controller
+public class BooksContoller {
+	private BookRepository bookRepository;
+
+	public BooksContoller(BookRepository bookRepository) {
+		super();
+		this.bookRepository = bookRepository;
+	}
+
+	@RequestMapping("/books")
+	public String getBooks(Model model) {
+		model.addAttribute("books", bookRepository.findAll());
+		return "books";
+	}
+}
