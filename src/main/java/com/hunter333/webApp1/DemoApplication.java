bab9a147aa93a3ca210ba5e38fa18f7541bf424d
Hunter333 <hunter333@abv.bg>
2018-08-29 23:06:26 +0300

    move demo application.properties

--- src/main/java/com/hunter333/webApp1/DemoApplication.java
+++ src/main/java/com/hunter333/webApp1/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.hunter333.webApp1.demo;
+package com.hunter333.webApp1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
